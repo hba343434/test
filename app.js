db27d92c1e8873306ddec59b63248fd3c139564c
hba343434 <120598843+hba343434@users.noreply.github.com>
2023-12-04 15:33:22 +0630

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    let box = $('.box');
+    let icon = $('.fab.fa-telegram');
+    
+    if( box.hover() ) {
+      // icon.removeClass('fa-telegram').addClass('fa-telegram-plane');
+    }
+    
+    
+  });
